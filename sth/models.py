e6a24cabde302b6b03b0df4fc705d9d1b010769a
jonghyunyang <jonghyun5646@naver.com>
2022-10-17 17:02:31 +0900

    DB 수정

--- sth/models.py
+++ sth/models.py
@@ -4,15 +4,20 @@ from django.db import models
 # Create your models here.
 class Multi(models.Model):
     id = models.AutoField(primary_key=True)
-    addr = models.CharField(max_length=500)
-    m_domain = models.IntegerField()
-    s_domain = models.IntegerField()
     area = models.FloatField()
     floor = models.IntegerField()
-    year = models.FloatField()
+    sigungu = models.CharField(max_length=500)
+    m_domain = models.IntegerField()
+    s_domain = models.IntegerField()
     latitude = models.FloatField()
     longitude = models.FloatField()
+    gu = models.CharField(max_length=500)
+    dong = models.CharField(max_length=500)
+    deposit = models.IntegerField()
+    predpr = models.IntegerField()
+    rate = models.FloatField()
+    risk = models.CharField(max_length=500)
 
     class Meta:
         db_table = 'multi'
-        ordering = ['-addr']
+        ordering = ['-id']
