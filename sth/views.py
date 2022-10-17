68be2e705cecafb38dd7328edc4467b5970b3b95
JJH00101 <107897703+JJH00101@users.noreply.github.com>
2022-10-17 14:34:56 +0900

    카카오맵 수정

--- sth/views.py
+++ sth/views.py
@@ -34,20 +34,27 @@ class MultiView(View):
     def post(self, request):
         gu = request.POST['gu']
         dong = request.POST['dong']
-        # searched = request.POST['searched']
+        searched = request.POST['searched']
         addr = gu + " " + dong
         multi = Multi.objects.select_related().filter(addr__contains=addr)
 
         try:
-            url = 'https://dapi.kakao.com/v2/local/search/address.json?query=' + addr
-            headers = {"Authorization": "KakaoAK 06b665abccae1b7ca9b1ad97a115b5e7"}
-            api_json = json.loads(str(requests.get(url, headers=headers).text))
-            address = api_json['documents'][0]['address']
-            crd = {"lat": address['y'], "lng": address['x']}
+            crd = find_crd(addr + searched)
 
         except Exception as e:
-            crd = {"lat": 37.4858838, "lng": 126.8973216, "error": e}
-
+            try:
+                crd = find_crd(addr)
+            except Exception as e:
+                crd = {"lat": 37.4858838, "lng": 126.8973216, "error": e}
 
         context = {'mul': multi, "crd": crd}
         return render(request, 'sth/multi.html', context)
+
+
+def find_crd(addr):
+    url = 'https://dapi.kakao.com/v2/local/search/address.json?query=' + addr
+    headers = {"Authorization": "KakaoAK 06b665abccae1b7ca9b1ad97a115b5e7"}
+    api_json = json.loads(str(requests.get(url, headers=headers).text))
+    address = api_json['documents'][0]['address']
+    crd = {"lat": address['y'], "lng": address['x'], "error": "fine"}
+    return crd
